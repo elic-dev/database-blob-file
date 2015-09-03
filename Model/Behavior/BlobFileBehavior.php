ff34e937085f10e551cc0765f24c7ee59e752a74
Matthias Lill <me@matthiaslill.de>
2015-09-03 17:30:53 +0200

    Error handling using validation function. Returns error message on failure.

--- Model/Behavior/BlobFileBehavior.php
+++ Model/Behavior/BlobFileBehavior.php
@@ -69,6 +69,10 @@ class BlobFileBehavior extends ModelBehavior {
 
 		$val = array_shift($params);
 
+		if ( ! empty($val['error'])) {
+			return $this->errorCodeToMessage($val['error']);
+		}
+
 		if ( ! empty($val['name']) and empty($val['tmp_name'])) {
 			return false;
 		}
@@ -81,6 +85,38 @@ class BlobFileBehavior extends ModelBehavior {
 		$fileHandler->loadFromFile($val['tmp_name']);
 
 		return $fileHandler->isImage();
+	}
+
+	protected function errorCodeToMessage($code) 
+	{ 
+		switch ($code) { 
+			case UPLOAD_ERR_INI_SIZE: 
+				$message = "The uploaded file exceeds the maximum allowed upload size of ".ini_get('upload_max_filesize').'.'; // upload_max_filesize directive in php.ini
+				break; 
+			case UPLOAD_ERR_FORM_SIZE: 
+				$message = "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form";
+				break; 
+			case UPLOAD_ERR_PARTIAL: 
+				$message = "The uploaded file was only partially uploaded"; 
+				break; 
+			case UPLOAD_ERR_NO_FILE: 
+				$message = "No file was uploaded"; 
+				break; 
+			case UPLOAD_ERR_NO_TMP_DIR: 
+				$message = "Missing a temporary folder"; 
+				break; 
+			case UPLOAD_ERR_CANT_WRITE: 
+				$message = "Failed to write file to disk"; 
+				break; 
+			case UPLOAD_ERR_EXTENSION: 
+				$message = "File upload stopped by extension"; 
+				break; 
+
+			default: 
+				$message = "Unknown upload error"; 
+				break; 
+		} 
+		return $message; 
 	}
 
 }
